f4e41a6746de23b46d61eef65afdf002fa05316f
muieer <yang_for_work@qq.com>
2023-09-05 20:29:45 +0800

    add spring mvc model

---
+++ spring_mvc_example/src/main/java/org/muieer/SpringMvcExample.java
@@ -0,0 +1,11 @@
+package org.muieer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcExample {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcExample.class);
+    }
+}
