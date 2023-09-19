d68af1ef3c91d30bf72e35e6e39ed452c8dbb1fc
muieer <yang_for_work@qq.com>
2023-09-19 20:23:37 +0800

    up

--- spring_scheduling_example/src/main/java/org/muieer/config/SchedulingConfiguration.java
+++ spring_scheduling_example/src/main/java/org/muieer/config/SchedulingConfiguration.java
@@ -10,6 +10,10 @@ import org.springframework.scheduling.annotation.Scheduled;
 
 import java.util.Date;
 
+/*
+* official document：https://docs.spring.io/spring-framework/reference/integration/scheduling.html
+* */
+
 @Configuration
 @EnableAsync
 @EnableScheduling
@@ -20,12 +24,13 @@ public class SchedulingConfiguration {
     @Async
     public void asyncRun() throws InterruptedException {
         Thread.sleep(10000);
-        LOGGER.info("SchedulingConfiguration asyncRun 方法调用执行结束");
+        LOGGER.info("SchedulingConfiguration asyncRun 方法异步调用执行结束");
     }
 
-    @Scheduled(cron = "30-59 * * * * *")
+    // 每分钟的第 15 到 30 秒会执行，间隔 10 秒执行一次，会在第 15、25 秒调度
+    @Scheduled(cron = "15-30/10 * * * * *")
     public void periodRun() {
-        LOGGER.info(new Date().toString());
+        LOGGER.info("执行时间是：" + new Date());
     }
 
 }
