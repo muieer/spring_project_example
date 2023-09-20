283c9f1a68371137f8568c4ff0bf0f1b41c963e0
muieer <yang_for_work@qq.com>
2023-09-20 19:57:39 +0800

    up

--- spring_scheduling_example/src/main/java/org/muieer/SpringSchedulingExampleApp.java
+++ spring_scheduling_example/src/main/java/org/muieer/SpringSchedulingExampleApp.java
@@ -3,6 +3,10 @@ package org.muieer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+* http://www.quartz-scheduler.org/documentation/quartz-2.3.0/
+*
+* */
 @SpringBootApplication
 public class SpringSchedulingExampleApp {
     public static void main(String[] args) {
