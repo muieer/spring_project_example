bca3f22dda597503dfaf3497979b6c2284aad34f
muieer <yang_for_work@qq.com>
2023-10-07 16:59:34 +0800

    support read custom properties

---
+++ spring_mvc_example/src/main/java/org/muieer/config/CustomProperties.java
@@ -0,0 +1,34 @@
+package org.muieer.config;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.Resource;
+import jakarta.inject.Inject;
+import lombok.Getter;
+import lombok.ToString;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+@ToString
+@Getter
+@Configuration
+@PropertySource("classpath:/custom.properties")
+public class CustomProperties {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomProperties.class);
+
+    @Autowired
+    private Environment env;
+
+    @Value("${spring.profiles.active}")
+    private String profile;
+
+    @PostConstruct
+    void init() {
+        LOGGER.info(this.toString());
+    }
+}
